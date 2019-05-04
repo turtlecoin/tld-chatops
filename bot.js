ab7d19f2735cebaf9d9b5f9eef1b05c38e33a3e0
Brandon Lehmann <brandonlehmann@gmail.com>
2019-05-04 09:55:46 -0400

    - More package.json fun - Pulled status channel out into config file - Added a few helper functions as almost every discord.js call is a promise that could reject (and the catch is a pain) - Fixed the logic of detecting the 'A, CNAME, TXT' - Cleaned up a few other small bits and pieces here and there

--- bot.js
+++ bot.js
@@ -1,106 +1,134 @@
-// Copyright (c) 2019, The TurtleCoin Developers
+// Copyright (c) 2019, TurtleCoin Developers
+//
+// Please see the included LICENSE file for more information.
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+'use strict'
 
-client.login(process.env.DISCORDTOKEN);
+const Config = require('./config.json')
+const Discord = require('discord.js')
+const util = require('util')
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("with DNS");
-    var statusChannel = client.channels.get("574186700021170186"); // TRTL NETWORK / DEV_DNS
-    // var statusChannel = client.channels.get("574171958980247562"); // STAGE SERVER / DNS_STATUS
-    statusChannel.send(
-        ":bee: :honey_pot: :fire: :wind_blowing_face: :drooling_face: "
-    );
-    console.log("Servers:");
-    client.guilds.forEach(guild => {
-        console.log(" - " + guild.name);
-    });
-});
+const client = new Discord.Client()
 
-function commandHandler(receivedMessage) {
-    let fullCommand = receivedMessage.content.substr(6);
-    let splitCommand = fullCommand.split(" ");
-    let primaryCommand = splitCommand[0];
-    let arguments = splitCommand.slice(1);
+if (!process.env.DISCORDTOKEN) {
+  throw new Error('You must supply the BOT Discord Token in the enviromental variable DISCORDTOKEN')
+}
 
-    console.log("\nCommand received: " + primaryCommand);
-    console.log("TYPE: " + arguments[0]);
-    console.log("DEST: " + arguments[1]);
-    console.log("SOURCE: " + arguments[2]);
+/* Helper functions that catch promise errors and always resolve with their status */
+function tryChannelSendMessage (channel, message) {
+  return new Promise((resolve, reject) => {
+    channel.send(message).then(() => {
+      return resolve(true)
+    }).catch(() => {
+      return resolve(false)
+    })
+  })
+}
 
-    var statusChannel = client.channels.get("574186700021170186"); // TRTL NETWORK / DEV_DNS
-    // var statusChannel = client.channels.get("574171958980247562"); // STAGE SERVER / DNS_STATUS
-    statusChannel.send(
-        "Message received from " +
-            receivedMessage.author.toString() +
-            ": " +
-            receivedMessage.content
-    );
+function tryMessageReact (message, reaction) {
+  return new Promise((resolve, reject) => {
+    message.react(reaction).then(() => {
+      return resolve(true)
+    }).catch(() => {
+      return resolve(false)
+    })
+  })
+}
 
-    if (primaryCommand == "help") {
-        helpCommand(receivedMessage);
-    } else if (primaryCommand == "register") {
-        registerCommand(arguments, receivedMessage);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+
+  client.user.setActivity('with DNS')
+
+  const statusChannel = client.channels.get(Config.channels.status)
+
+  tryChannelSendMessage(statusChannel, ':bee: :honey_pot: :fire: :wind_blowing_face: :drooling_face:').then(success => {
+    if (success) {
+      console.log('Servers:')
+      client.guilds.forEach(guild => console.log(' - ' + guild.name))
     } else {
-        receivedMessage.channel.send("That's not a command I understand yet.");
+      console.warn('Could not send a message to the configured status channel')
     }
+  })
+})
+
+client.on('message', receivedMessage => {
+  // const statusChannel = client.channels.get(Config.channels.status)
+
+  if (receivedMessage.author === client.user) {
+    return
+  }
+
+  if (receivedMessage.content.startsWith('.trtl')) {
+    commandHandler(receivedMessage)
+  }
+
+  if (receivedMessage.content === 'ping') {
+    const currentdate = Date()
+    tryMessageReact(receivedMessage, '🐢')
+    console.log(currentdate, '\n PONG')
+  }
+})
+
+// Login after we have defined our events to make sure that we catch all of them
+client.login(process.env.DISCORDTOKEN)
+
+function commandHandler (receivedMessage) {
+  let fullCommand = receivedMessage.content.substr(6)
+  let splitCommand = fullCommand.split(' ')
+  let primaryCommand = splitCommand[0]
+
+  /* Find our arguments and drop them all to lowercase for easier matching later */
+  let args = splitCommand.slice(1)
+
+  console.log('\nCommand received: ' + primaryCommand)
+  console.log('TYPE: ' + args[0])
+  console.log('DEST: ' + args[1])
+  console.log('SOURCE: ' + args[2])
+
+  const statusChannel = client.channels.get(Config.channels.status)
+
+  tryChannelSendMessage(statusChannel, util.format('Message received from %s: %s', receivedMessage.author.toString(), receivedMessage.content))
+
+  switch (primaryCommand.toLowerCase()) {
+    case 'help':
+      return helpCommand(receivedMessage)
+    case 'register':
+      return registerCommand(args, receivedMessage)
+    default:
+      return tryChannelSendMessage(receivedMessage.channel, 'That is not a command I understand yet')
+  }
 }
 
-client.on("message", receivedMessage => {
-    var statusChannel = client.channels.get("574186700021170186"); // TRTL NETWORK / DEV_DNS
-    // var statusChannel = client.channels.get("574171958980247562"); // STAGE SERVER / DNS_STATUS
-    if (receivedMessage.author == client.user) {
-        return;
-    }
-
-    if (receivedMessage.content.startsWith(".trtl")) {
-        commandHandler(receivedMessage);
-    }
-
-    if (receivedMessage.content === "ping") {
-        currentdate = Date();
-        receivedMessage.react("🐢");
-        console.log(currentdate, "\n PONG");
-    }
-});
-
-function helpCommand(receivedMessage) {
-    receivedMessage.channel.send(
-        "HELP!\nActivate me with **.trtl <command>**\nCommands: `register`, `help`"
-    );
+function helpCommand (receivedMessage) {
+  tryChannelSendMessage(receivedMessage.channel, 'HELP!\nActivate me with **.trtl <command>**\nCommands: `register`, `help`')
 }
 
-function registerCommand(arguments, receivedMessage) {
-    if (arguments.length < 3) {
-        receivedMessage.channel.send(
-            "Not enough values to register. The syntax should be `.trtl register <TYPE> <DESTINATION> <SOURCE/VALUE>`"
-        );
-        receivedMessage.react("💩");
-    }
-    if (arguments.length > 3) {
-        receivedMessage.channel.send(
-            "Too many values to register. The syntax should be `.trtl register <TYPE> <DESTINATION> <SOURCE/VALUE>`"
-        );
-        receivedMessage.react("💩");
-    }
-    if (arguments.length === 3) {
-        receivedMessage.channel.send("number of args is correct");
-    }
+function registerCommand (args, receivedMessage) {
+  if (args.length < 3) {
+    tryChannelSendMessage(receivedMessage.channel, 'Not enough values to register. The syntax should be `.trtl register <TYPE> <DESTINATION> <SOURCE/VALUE>`')
+    return tryMessageReact(receivedMessage, '💩')
+  }
+  if (args.length > 3) {
+    tryChannelSendMessage(receivedMessage.channel, 'Too many values to register. The syntax should be `.trtl register <TYPE> <DESTINATION> <SOURCE/VALUE>`')
+    return tryMessageReact(receivedMessage, '💩')
+  }
+  if (args.length === 3) {
+    tryChannelSendMessage(receivedMessage.channel, 'Number of arguments is correct')
+  }
 
-    // TODO check if argument for type is one of the options
-    if (arguments[0].includes(recordTypes.some())) {
-        var recordTypes = ["A", "TXT", "CNAME"];
-        receivedMessage.channel.send(
-            "This is not a record type I understand yet. Options: `A`, `TXT`, `CNAME`."
-        );
-        receivedMessage.react("💩");
-    }
-    if (arguments[0] === recordTypes.some()) {
-        receivedMessage.channel.send("This is fine.");
-    }
+  const recordTypes = ['A', 'TXT', 'CNAME']
+
+  /* Verify that our first argument is indeed one of the record types that
+     we know how to handle or exit early */
+  if (recordTypes.indexOf(args[0].toUpperCase()) === -1) {
+    tryChannelSendMessage(receivedMessage.channel, 'This is not a record type I understand yet. Options: `A`, `TXT`, `CNAME`.')
+    return tryMessageReact(receivedMessage, '💩')
+  }
+
+  tryChannelSendMessage(receivedMessage.channel, 'This is fine')
 }
+
 /*
 spendBack() {
 
@@ -109,7 +137,6 @@ spendBack() {
   - send min tx to trtlbot++ with payload amount as fee
   - delay 30s
   - next payload until spendBack is empty
- 
 
 }
 */
